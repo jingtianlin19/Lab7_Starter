f8742e68f639f6fe8c045b8eb5eedeee3afa3a2c
unknown <jil135@ucsd.edu>
2021-11-07 21:09:48 -0800

    update

--- sw.js
+++ sw.js
@@ -2,6 +2,15 @@
 //         so do not move it next to the other scripts
 
 const CACHE_NAME = 'lab-7-starter';
+const urlsToCache =  [
+'/Lab7_Starter/',
+'https://introweb.tech/assets/json/ghostCookies.json',
+'https://introweb.tech/assets/json/birthdayCake.json',
+'https://introweb.tech/assets/json/chocolateChip.json',
+'https://introweb.tech/assets/json/stuffing.json',
+'https://introweb.tech/assets/json/turkey.json',
+'https://introweb.tech/assets/json/pumpkinPie.json'
+];
 
 // Once the service worker has been installed, feed it some initial URLs to cache
 self.addEventListener('install', function (event) {
